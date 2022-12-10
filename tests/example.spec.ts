dd3e1abe07c416c9c8cbf1e8a74d19dc6d6afd1d
georgielink <georgielink01@gmail.com>
2022-12-10 22:39:04 +0800

    Add files via upload

---
+++ tests/example.spec.ts
@@ -0,0 +1,9 @@
+import { test, expect } from '@playwright/test'
+
+//To create a test
+test("Simple test", async({page})=>{
+    await page.goto('https:www.example.com')
+    const pageTitle= await page.locator('h1')
+    await expect(pageTitle).toContainText('Example Domain')
+
+})
